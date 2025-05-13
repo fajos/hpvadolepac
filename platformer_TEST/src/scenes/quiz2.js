bc78163ea30607ef98125067a80f1eb9532f26e8
fajos <34520219+fajos@users.noreply.github.com>
2025-05-13 17:02:17 +0100

    Update quiz2.js

--- platformer_TEST/src/scenes/quiz2.js
+++ platformer_TEST/src/scenes/quiz2.js
@@ -97,8 +97,13 @@ class quiz2 extends Phaser.Scene {
 	}
 	
 	lose = () => {
-		this.scene.start("GameOver");
-	}
+   		window.parent.postMessage(
+		     { event: 'retry', level: 'level2', attempt: this.scene.get('Level').currentAttempt },
+     			'*'
+   		);
+
+    		this.scene.start("GameOver");
+		}
 	
 	continue = () => {
 		this.scene.resume("Level");
