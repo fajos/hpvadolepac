e5bcfa321f87c468d6ab149a778e12069b8f7248
fajos <34520219+fajos@users.noreply.github.com>
2025-05-13 17:39:02 +0100

    Update quiz2.js

--- platformer_TEST/src/scenes/quiz2.js
+++ platformer_TEST/src/scenes/quiz2.js
@@ -106,10 +106,20 @@ class quiz2 extends Phaser.Scene {
 		}
 	
 	continue = () => {
-		this.scene.resume("Level");
-		this.scene.stop();
-	}
+  	// reset the attempt counter in Level
+  		const level = this.scene.get("Level");
+  		level.currentAttempt = 1;
 
+  	// tell the parent we’ve started level3
+  		window.parent.postMessage(
+    	{ event: 'level_started', level: 'level3', attempt: 1 },
+    	'*'
+  	);
+
+  	// resume & close
+  	this.scene.resume("Level");
+  	this.scene.stop();
+	}
 	/* END-USER-CODE */
 }
 
