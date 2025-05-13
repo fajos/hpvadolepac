22642711abbce259def9e62d322690fa6259783b
fajos <34520219+fajos@users.noreply.github.com>
2025-05-13 19:29:41 +0100

    Update quiz1.js

--- platformer_TEST/src/scenes/quiz1.js
+++ platformer_TEST/src/scenes/quiz1.js
@@ -97,6 +97,8 @@ class quiz1 extends Phaser.Scene {
 	}
 	
 	 lose = () => {
+		const lvl = this.scene.get("Level");
+  		lvl.currentAttempt++; 
    		window.parent.postMessage(
 		     { event: 'retry', level: 'level1', attempt: this.scene.get('Level').currentAttempt },
      			'*'
@@ -107,12 +109,12 @@ class quiz1 extends Phaser.Scene {
 	
 	continue = () => {
   	// reset the attempt counter in Level
-  		const level = this.scene.get("Level");
-  		level.currentAttempt = 1;
+  		const lvl = this.scene.get("Level");
+  		lvl.currentAttempt = 0;
 
   	// tell the parent we’ve started level2
   		window.parent.postMessage(
-    	{ event: 'level_started', level: 'level2', attempt: 1 },
+    	{ event: 'level_started', level: 'level2', attempt: 0 },
     	'*'
   	);
 
